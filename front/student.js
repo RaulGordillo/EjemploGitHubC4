64ba45dcd4537624f24ba480a3586394925e6ca1
Raúl <ragordilloz@gmail.com>
2022-10-10 21:25:29 -0500

    first commit

---
+++ front/student.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class student extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onsubmit(event) {}
+    
+  render() {
+    return <div>student</div>
+    
+  }
+}
